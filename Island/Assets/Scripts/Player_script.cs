4052c7f6de5980fb8a4ec2ac1e2cc35d82393ec9
emc-vb <evavonbutler@yahoo.de>
2022-06-21 15:33:14 +0200

    Player movement + Level 2
    - level 2 ist nur erstellt aber noch nicht designed und hat keine funktionen
    - animation bei player movement funktioniert noch nicht

--- Island/Assets/Scripts/Player_script.cs
+++ Island/Assets/Scripts/Player_script.cs
@@ -4,24 +4,80 @@ using UnityEngine;
 
 public class Player_script : MonoBehaviour
 {
-    [SerializeField]
-    public CharacterController controller;
 
     [SerializeField]
     public float _moveSpeed = 1f;
 
+    [SerializeField]
+    public float _turnSpeed = 15f;
+
+    [SerializeField]
+    private Rigidbody RB;
+
+    private float _jumpingSpeed = 10f;
+
+
+    // animation for walking
+    private Animator animator;
+
+
+    void Start() {
+
+        animator = GetComponent<Animator>();
+    }
+
     // Update is called once per frame
-    void Update()
-    {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+    void Update() {
 
-        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+        
 
-        if (direction.magnitude >= 0.1f)
+        // horizontal player movement: rotates the player on the y-axis
+        float horizontal = Input.GetAxis("Horizontal");
+
+        if (horizontal > 0)
         {
-            controller.Move(direction * _moveSpeed * Time.deltaTime);
+            //transform.Translate(Vector3.right * _moveSpeed * Time.deltaTime);
+            transform.Rotate(0, horizontal * _turnSpeed, 0);
         }
+
+        if (horizontal < 0)
+        {
+            //transform.Translate(Vector3.left * _moveSpeed * Time.deltaTime);
+            transform.Rotate(0, horizontal, 0);
+        }
+
+
+        // vertical player movement: moves player forward or back
+        float vertical = Input.GetAxis("Vertical");
+
+        if (vertical > 0)
+        {
+            transform.Translate(Vector3.forward * _moveSpeed * Time.deltaTime);
+        }
+
+        if (vertical < 0)
+        {
+            transform.Translate(Vector3.back * _moveSpeed * Time.deltaTime);
+        }
+
+        // create movementDirection to check if player isMoving
+        Vector3 movementDirection = new Vector3(horizontal, 0, vertical);
+
+        if (movementDirection != Vector3.zero)
+        {
+            animator.SetBool("isMoving", true);
+        }
+        else
+        {
+            animator.SetBool("isMoving",false);
+        }
+
+        // player jumps
+        if (Input.GetKeyDown("space"))
+        {
+            RB.velocity += new Vector3(0f, _jumpingSpeed, 0f);
+        }
+
     }
 
 }
