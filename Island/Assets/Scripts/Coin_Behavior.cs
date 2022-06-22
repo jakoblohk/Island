dee81f60d0c8deb2ef884f0711bec96092f72ad0
Jakob <jakoblohkamp@gmail.com>
2022-06-22 17:26:32 +0200

    latest update -broken player

--- Island/Assets/Scripts/Coin_Behavior.cs
+++ Island/Assets/Scripts/Coin_Behavior.cs
@@ -7,6 +7,9 @@ public class Coin_Behavior : MonoBehaviour
     [SerializeField]
     public int rotateSpeed = 5;
 
+    [SerializeField]
+    private Player_script _player;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +19,18 @@ public class Coin_Behavior : MonoBehaviour
     void Update()
     {
         transform.Rotate(0, 0, 50 * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Coin collision");
+            Destroy(this.gameObject);
+            // coins++ in player
+            _player.Coins();
+        }
+
     }
 }
--- Island/Assets/Scripts/Player_script.cs
+++ Island/Assets/Scripts/Player_script.cs
@@ -6,7 +6,7 @@ public class Player_script : MonoBehaviour
 {
 
     [SerializeField]
-    public float _moveSpeed = 1f;
+    public float _moveSpeed = 2f;
 
     [SerializeField]
     public float _turnSpeed = 15f;
@@ -14,23 +14,37 @@ public class Player_script : MonoBehaviour
     [SerializeField]
     private Rigidbody RB;
 
-    private float _jumpingSpeed = 10f;
+    [SerializeField]
+    private float _jumpingSpeed = 4f;
 
+    private float _nextJumpTime;
+    private float _coolDownTime = 1f;
+    private int _coins;
 
     // animation for walking
-    private Animator animator;
+    //private Animator animator;
 
 
     void Start() {
 
-        animator = GetComponent<Animator>();
+        //transform.position = new Vector3(-18f, 1f, -12f);
+        //animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update() {
 
-        
+        PlayerMovement();
 
+    }
+
+    // Coin counter
+    public void Coins()
+    {
+        _coins++;
+    }
+    void PlayerMovement()
+    {
         // horizontal player movement: rotates the player on the y-axis
         float horizontal = Input.GetAxis("Horizontal");
 
@@ -38,46 +52,49 @@ public class Player_script : MonoBehaviour
         {
             //transform.Translate(Vector3.right * _moveSpeed * Time.deltaTime);
             transform.Rotate(0, horizontal * _turnSpeed, 0);
-        }
-
-        if (horizontal < 0)
+        } else if (horizontal < 0)
         {
             //transform.Translate(Vector3.left * _moveSpeed * Time.deltaTime);
             transform.Rotate(0, horizontal, 0);
         }
 
-
         // vertical player movement: moves player forward or back
         float vertical = Input.GetAxis("Vertical");
 
         if (vertical > 0)
-        {
+        {   
             transform.Translate(Vector3.forward * _moveSpeed * Time.deltaTime);
-        }
-
-        if (vertical < 0)
+        } else if (vertical < 0)
         {
             transform.Translate(Vector3.back * _moveSpeed * Time.deltaTime);
         }
+        
+
+        // JUMPING
+        if (Input.GetKeyDown("space") && _nextJumpTime < Time.time)
+        {
+            Debug.Log("jump");
+            RB.velocity += new Vector3(0f, _jumpingSpeed, 0f);
+            _nextJumpTime = Time.time + _coolDownTime;
+        }
+
+        // TELEPORT - if player is falling teleport him back to a certain position
+        if (transform.position.y < -10)
+        {
+            transform.position = new Vector3(0f, 2f, 0f);
+        }
 
         // create movementDirection to check if player isMoving
-        Vector3 movementDirection = new Vector3(horizontal, 0, vertical);
-
-        if (movementDirection != Vector3.zero)
+        //Vector3 movementDirection = new Vector3(horizontal, 0, vertical);
+        /*if (movementDirection != Vector3.zero)
         {
             animator.SetBool("isMoving", true);
         }
         else
         {
-            animator.SetBool("isMoving",false);
-        }
-
-        // player jumps
-        if (Input.GetKeyDown("space"))
-        {
-            RB.velocity += new Vector3(0f, _jumpingSpeed, 0f);
-        }
-
+            Debug.Log("not moving");
+            animator.SetBool("isMoving", false);
+        }*/
     }
 
 }
